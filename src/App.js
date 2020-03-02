8bf0510f3a51c4103aabb276ff5f05ffd38f110a
Darshana <darshana_tak@persistent.com>
2020-03-02 11:47:45 +0530

    First commit for shop destination react site

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Homepage from './homepage.component';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Homepage />
+      </div>
+    );
+  }
+}
+
+export default App;
