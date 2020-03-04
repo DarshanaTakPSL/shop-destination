ab0113ec180e30fc99325e95f31d18680cfd367c
Darshana <darshana_tak@persistent.com>
2020-03-04 17:52:52 +0530

    Added collection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import Homepage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
 
-import './App.css';
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Homepage />
+      <div>
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/shop' component={ShopPage} />
+        </Switch>
       </div>
     );
   }
